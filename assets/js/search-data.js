ff9b3091f929813c3f99571fdc6b34e09480e6c5
QilinLi147 <liqilin147@gmail.com>
2025-12-19 08:04:37 +0000

    Deploying to gh-pages from @ QilinLi147/qilin@7c6009bc6b8f562fdc2d0ef511738d4ad022ef47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,11 +418,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/qilin/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/qilin/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -431,6 +426,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/qilin/news/announcement_1/";
+            },},{id: "news-music-emotion-recognition-paper-accepted-in-ieee-taffc",
+          title: 'Music Emotion Recognition Paper Accepted in IEEE TAFFC',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/qilin/news/announcement_2/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
